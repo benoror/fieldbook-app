fcee50b33cf0486815f16a23bb831055e28c2191
Benjamin Orozco <benoror@gmail.com>
2016-02-03 16:19:43 -0600

    refactor(inventory): add query params

--- app/models/product.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  id: DS.attr(),
-  name: DS.attr()
-});
--- app/routes/inventory.js
+++ app/routes/inventory.js
@@ -3,6 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   fieldbook: Ember.inject.service('fieldbook'),
   model() {
-    return this.get('fieldbook').request('/inventory');
+    const endpoint = '/inventory';
+    const include = [
+      'productid',
+      'quantity',
+      'restock_at'
+    ].join(',');
+    const limit = 10;
+    const query = `${endpoint}?include=${include}&limit=${limit}`
+
+    return this.get('fieldbook').request(query);
   }
 });
